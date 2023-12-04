abd69f89d6cc00ef3b1d9de6419955bd480e5ee1
Konrad Geller <konrad.geller@rwth-aachen.de>
2023-12-04 20:44:41 +0100

    cleanup

--- solutions/day4.py
+++ solutions/day4.py
@@ -2,36 +2,31 @@ import re
 import numpy as np
 
 
-def prepare(input: str):
-    lines = input.split("\n")
-    matchesPerCard = np.array([0] * len(lines))
-    for card in lines:
-        number, winning, nums = re.split(r"[:|]", card)
-        number = int(re.split(r" +", number)[1])
-        winning = list(map(lambda x: int(x), re.split(r" +", winning.strip())))
-        nums = list(map(lambda x: int(x), re.split(r" +", nums.strip())))
-        matches = 0
-        for num in nums:
-            if num in winning:
-                matches += 1
-        matchesPerCard[number - 1] = matches
+def countMatches(input: str):
+    lines = input.splitlines()
+    matchesPerCard = np.full(len(lines), 0)
+    for idx, card in enumerate(lines):
+        _, winning, nums = re.split(r"[:|]", card)
+        winning = [int(x) for x in winning.strip().split()]
+        nums = [int(x) for x in nums.strip().split()]
+        matches = np.sum([1 for num in nums if num in winning])
+        matchesPerCard[idx] = matches
 
     return matchesPerCard
 
 
 def part1(input: str):
-    matchesPerCard = prepare(input)
+    matchesPerCard = countMatches(input)
     return np.sum([pow(2, matches - 1) for matches in matchesPerCard if matches > 0])
 
 
 def part2(input):
-    matchesPerCard = prepare(input)
-    cardStock = np.array([1] * len(matchesPerCard))
-    for num in range(len(matchesPerCard)):
-        cardsToScratch = cardStock[num]
-        matches = matchesPerCard[num]
-        # add <cardsToScratch> to the next <matches> cards
-        start = num + 1
+    matchesPerCard = countMatches(input)
+    cardStock = np.full(len(matchesPerCard), 1)
+    for cardIdx in range(len(matchesPerCard)):
+        cardsToScratch = cardStock[cardIdx]
+        matches = matchesPerCard[cardIdx]
+        start = cardIdx + 1
         end = min(start + matches, len(cardStock))
         cardStock[start:end] = np.add(
             cardStock[start:end], np.full(end - start, cardsToScratch)
